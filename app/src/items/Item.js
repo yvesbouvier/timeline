069d1bd10d5aab9afe675070f6fc7759e181a014
YvesL <yvesbouvier@gmail.com>
2015-12-21 16:36:25 -0800

    cleanup

--- app/src/items/Item.js
+++ app/src/items/Item.js
@@ -5,12 +5,6 @@
   angular.module('item', [ 'ngMaterial', 'uiGmapgoogle-maps']).config(function(uiGmapGoogleMapApiProvider) {
 
 
-    uiGmapGoogleMapApiProvider.configure({
-      key: 'AIzaSyAFJjUaUQy574SfQ_i4RBwdloDVhjxOU5I',
-      v: '3.X',
-      libraries: 'places'
-    });
-
   });
 
   })();
